0ad67315af32d5dc2aec80d73bde81004486e028
Rana-Arslan10 <codeaddictors10@gmail.com>
2023-08-13 15:23:25 +0500

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    
+    <div>
+      <center><h1>Hello world</h1></center>
+      
+    </div>
+  )
+}
